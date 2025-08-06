e69578e3ce7cf61ff0d2fb69feb4d647658328b2
Md. Jahidul Islam <jahidmunshi99@gmail.com>
2025-08-06 23:30:19 +0600

    update

--- src/data/index.js
+++ src/data/index.js
@@ -7,73 +7,31 @@ const allVideos = [
     batch: "4",
     link: "https://www.youtube.com/embed/YK8vfjwZjjs?si=KI022UOwW8W_cyyh",
   },
-  {
-    id: crypto.randomUUID(),
-    title: "Class-2 | How to install Software | How to use MS word",
-    description: "Class-2 | How to install Software | How to use MS word",
-    batch: "4",
-    link: "2",
-  },
-  {
-    id: crypto.randomUUID(),
-    title:
-      "Class-3 | How to convert PDF to word and How to clean any document using word",
-    description: "",
-    batch: "4",
-    link: "3",
-  },
-  {
-    id: crypto.randomUUID(),
-    title:
-      "Class-4 | How to clean a Pdf file | How many parts in a book | 4 Batch",
-    description: "",
-    batch: "4",
-    link: "4",
-  },
+
   {
     id: crypto.randomUUID(),
     title:
       "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
-    description: "",
+    description:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
     batch: "4",
-    link: "5",
-  },
-  {
-    id: crypto.randomUUID(),
-    title: "Class-6 | How to customize Heading-1, Importance of Stylesheet",
-    description: "",
-    batch: "4",
-    link: "6",
+    link: "https://www.youtube.com/embed/o46Tk47_O40?si=QWvIRue1Rfthz7OC",
   },
   {
     id: crypto.randomUUID(),
     title: "How to use google meet",
-    description: "How to use google meet",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eveniet culpa inventore vero et consequatur deleniti. Dolore quibusdam facilis nisi! Laborum vel maiores officia modi quo placeat nulla, accusantium laboriosam. Tenetur quo quibusdam dolores iste ab dignissimos numquam in vitae repellat sunt amet repellendus ad sit pariatur, ratione aliquam alias atque quos magnam ut. Ducimus, officia. Commodi laborum ut in corrupti dolorem placeat aperiam, modi nesciunt aspernatur veritatis fugiat? Sunt, voluptate esse. Quo eum corporis tenetur laudantium cum. Deserunt, quos, a omnis fuga expedita esse ullam dicta vel eveniet ipsum similique. Sit dignissimos rerum tenetur ab consequuntur pariatur eius quidem.z",
     batch: "4",
-    link: "7",
+    link: "https://www.youtube.com/embed/YK8vfjwZjjs?si=KI022UOwW8W_cyyh",
   },
   {
     id: crypto.randomUUID(),
-    title: "Class-2 | How to install Software | How to use MS word",
-    description: "Class-2 | How to install Software | How to use MS word",
+    title: "How to use google meet",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eveniet culpa inventore vero et consequatur deleniti. Dolore quibusdam facilis nisi! Laborum vel maiores officia modi quo placeat nulla, accusantium laboriosam. Tenetur quo quibusdam dolores iste ab dignissimos numquam in vitae repellat sunt amet repellendus ad sit pariatur, ratione aliquam alias atque quos magnam ut. Ducimus, officia. Commodi laborum ut in corrupti dolorem placeat aperiam, modi nesciunt aspernatur veritatis fugiat? Sunt, voluptate esse. Quo eum corporis tenetur laudantium cum. Deserunt, quos, a omnis fuga expedita esse ullam dicta vel eveniet ipsum similique. Sit dignissimos rerum tenetur ab consequuntur pariatur eius quidem.z",
     batch: "4",
-    link: "8",
-  },
-  {
-    id: crypto.randomUUID(),
-    title:
-      "Class-3 | How to convert PDF to word and How to clean any document using word",
-    description: "",
-    batch: "4",
-    link: "9",
-  },
-  {
-    id: crypto.randomUUID(),
-    title:
-      "Class-4 | How to clean a Pdf file | How many parts in a book | 4 Batch",
-    description: "",
-    batch: "4",
-    link: "10",
+    link: "https://www.youtube.com/embed/YK8vfjwZjjs?si=KI022UOwW8W_cyyh",
   },
   {
     id: crypto.randomUUID(),
@@ -86,10 +44,39 @@ const allVideos = [
   },
   {
     id: crypto.randomUUID(),
-    title: "Class-6 | How to customize Heading-1, Importance of Stylesheet",
-    description: "",
+    title:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    description:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
     batch: "4",
-    link: "https://u.pcloud.link/publink/show?code=XZjpHW5ZpvARxRsVR44JVMp49sv4jSyhFwDX",
+    link: "https://www.youtube.com/embed/o46Tk47_O40?si=QWvIRue1Rfthz7OC",
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    description:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    batch: "4",
+    link: "https://www.youtube.com/embed/o46Tk47_O40?si=QWvIRue1Rfthz7OC",
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    description:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    batch: "4",
+    link: "https://www.youtube.com/embed/o46Tk47_O40?si=QWvIRue1Rfthz7OC",
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    description:
+      "Class-5 | How to page setup, Margin Setup, Gutter Setup, | How to custom Heading-1 | 4 Batch",
+    batch: "4",
+    link: "https://www.youtube.com/embed/o46Tk47_O40?si=QWvIRue1Rfthz7OC",
   },
 ];
 
